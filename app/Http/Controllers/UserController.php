b1423bfac823a691d0c4ec2a9d714bdcf3bd3ec0
eus <minhtq9700@gmail.com>
2023-03-29 14:53:50 +0700

    ok

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use stdClass;
+// use stdClass;
 
 function checkDupName($array, $name)
 {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+// use App\Http\Controllers\UserController;
+use App\Http\Controllers\ImageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +14,13 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [UserController::class, 'index']);
-Route::get('/go-to-add-user', [UserController::class, 'goToAddUser']);
-Route::get('/go-to-update-user/{id}', [UserController::class, 'goToUpdateUser'])->name('to.users.update');
-Route::post('/add-user', [UserController::class, 'addNewUser']);
-Route::post('/update/{id}', [UserController::class, 'editUser'])->name('users.edit');
-Route::delete('/delete/{id}', [UserController::class, 'deleteUser'])->name('users.destroy');
+// Route::get('/', [UserController::class, 'index']);
+// Route::get('/go-to-add-user', [UserController::class, 'goToAddUser']);
+// Route::get('/go-to-update-user/{id}', [UserController::class, 'goToUpdateUser'])->name('to.users.update');
+// Route::post('/add-user', [UserController::class, 'addNewUser']);
+// Route::post('/update/{id}', [UserController::class, 'editUser'])->name('users.edit');
+// Route::delete('/delete/{id}', [UserController::class, 'deleteUser'])->name('users.destroy');
+
+Route::get('/image', [ImageController::class, 'index'])->name('image.index');
+Route::post('/image', [ImageController::class, 'store'])->name('image.store');
+Route::get('/allImages', [ImageController::class, 'show'])->name('image.index');
